520d017e38f8677783af528da9e766c5c107eb29
pearl41 <mosespearl22@gmail.com>
2022-10-24 15:49:56 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifindef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
@@ -9,7 +9,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a. int n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *scr);
 
-#endi|f
+#endif
